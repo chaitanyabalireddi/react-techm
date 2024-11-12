adec3933852d363fe6db95af018569550c2185e5
chaitanyabalireddi <ballireddychaitanya@gmail.com>
2024-11-12 18:52:53 +0530

    svg add

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,10 @@ export default {
   ],
   theme: {
     extend: {
-
+            fontFamily: {
+              fraktur: ['UnifrakturMaguntia', 'serif'],
+              sans: ['Roboto', 'sans-serif'], 
+            },
     },
   },
   plugins: [],
